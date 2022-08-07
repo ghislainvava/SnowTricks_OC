1d57fc1ab445c6f42baf1fd8f8df3803fa6b81a4
ghislainvava <ghislainvachet@yahoo.fr>
2022-08-07 17:04:07 +0200

    supp

--- src/Controller/SnowtricksController.php
+++ src/Controller/SnowtricksController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Figure;
 use App\Entity\Comment;
+use App\Entity\Category;
 use App\Form\CommentType;
-use App\Repository\CategoryRepository;
+use App\Form\FigureType;
 use App\Repository\FigureRepository;
-use Doctrine\ORM\EntityManagerInterface; //remplace ObjectManager
+use App\Repository\CategoryRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Doctrine\ORM\EntityManagerInterface; //remplace ObjectManager
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SnowtricksController extends AbstractController
 {
@@ -28,29 +31,53 @@ class SnowtricksController extends AbstractController
         ]);
     }
 
-    #[Route('/snowtricks/newfigure', name: 'snowstricks_create')] //important! order this route before {id}
-    #[Route('/snowtricks/{id}/edit', name: 'snowstricks_edit')]
-    public function form(Figure $figure = null, Request $request, EntityManagerInterface $manager): Response
-    {
-        if (!$figure) {
-        }
-        
-        $form = $this->createFormBuilder($figure)
-                    ->add('name')
-                    ->add('image')
-                    ->add('content')
-                    ->getForm();
-        $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $manager->persist($figure);
-            $manager->flush();
-            return $this->redirectToRoute('figure_show', ['id' => $figure->getId()]);
-        }
-        return $this->render('snowtricks/create.html.twig', [
-            'formFigure' => $form->createView()
+    // #[Route('/snowtricks/newfigure', name: 'snowstricks_create')] //important! order this route before {id}
+    // #[Route('/snowtricks/{id}/edit', name: 'snowstricks_edit')]
+    // public function form(Figure $figure = null, Request $request, EntityManagerInterface $manager): Response
+    // {
+    //     if (!$figure) {
+    //         $figure = new Figure();
+    //     }
+
+    //     $form = $this->createFormBuilder($figure)
+    //                 ->add('groupe', Category::class)
+    //                 ->add('name')
+    //                 ->add('image')
+    //                 ->add('content')
+    //                 ->getForm();
+    //     $form->handleRequest($request);
+
+    //     if ($form->isSubmitted() && $form->isValid()) {
+    //         $manager->persist($figure);
+    //         $manager->flush();
+    //         return $this->redirectToRoute('figure_show', ['id' => $figure->getId()]);
+    //     }
+    //     return $this->render('snowtricks/create.html.twig', [
+    //         'formFigure' => $form->createView()
+    //          ]);
+    // }
+
+    #[Route('/snowstricks/addfigure', name: 'add_figure')]
+    public function addFigure(EntityManagerInterface $manager, Request $request): Response
+    {
+        $user = $this->getUser();
+        $figure = new Figure();
+        $figure->getUser($user);
+
+        $form = $this->createForm(FigureType::class, $figure);
+        $form->handleRequest($request);
+        $figure = $form->getData();
+        $manager->persist($figure);
+        $manager->flush();
+
+
+
+        return $this->render('snowtricks/createFigure.html.twig', [
+            'formCreateFigure' => $form->createView()
              ]);
     }
+
 
     #[Route('/snowtricks/{id}', name: 'figure_show')]
     public function show($id, FigureRepository $repo): Response
@@ -64,13 +91,16 @@ class SnowtricksController extends AbstractController
         ]);
     }
 
-    #[Route('/delete/{id}', name: 'snowstrick_delete')]
-    public function deletePost(Figure $figure, Request $request, FigureRepository $figureRepository, EntityManagerInterface $mananger): Response  //RedirectResponse
+    #[Route('/delete/{id}', name: 'snowtrick_delete')]
+    public function deletePost(Figure $figure, Request $request, FigureRepository $figureRepository, EntityManagerInterface $manager): Response  //RedirectResponse
     {
         $user = $this->getUser();
         $figure = $figureRepository->find($request->get('id'));
+        $manager->remove($figure);
+        $manager->flush();
 
+        $this->addFlash('sucess', 'la figure a bien été supprimée');
 
-        return $this->redirectToRoute("Home");
+        return $this->redirectToRoute("home");
     }
 }
