d7bd98061d8a0ddd0e922f149b70f60fc0dfc0fd
ghislainvava <ghislainvachet@yahoo.fr>
2022-08-09 10:06:12 +0200

    en attente

--- src/Controller/SnowtricksController.php
+++ src/Controller/SnowtricksController.php
@@ -9,6 +9,7 @@ use App\Form\CommentType;
 use App\Form\FigureType;
 use App\Repository\FigureRepository;
 use App\Repository\CategoryRepository;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,6 +17,7 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Doctrine\ORM\EntityManagerInterface; //remplace ObjectManager
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormTypeInterface;
 
 class SnowtricksController extends AbstractController
 {
@@ -32,44 +34,46 @@ class SnowtricksController extends AbstractController
     }
 
 
-    // #[Route('/snowtricks/newfigure', name: 'snowstricks_create')] //important! order this route before {id}
-    // #[Route('/snowtricks/{id}/edit', name: 'snowstricks_edit')]
-    // public function form(Figure $figure = null, Request $request, EntityManagerInterface $manager): Response
-    // {
-    //     if (!$figure) {
-    //         $figure = new Figure();
-    //     }
+    #[Route('snowtricks/newfigure', name: 'snowtrick_create')] //important! order this route before {id}
+    #[Route('/snowtricks/{id}/edit', name: 'snowtrick_edit')]
+    public function form(Figure $figure = null, Request $request, EntityManagerInterface $manager): Response
+    {
+        if (!$figure) {
+            $figure = new Figure();
+        }
 
-    //     $form = $this->createFormBuilder($figure)
-    //                 ->add('groupe', Category::class)
-    //                 ->add('name')
-    //                 ->add('image')
-    //                 ->add('content')
-    //                 ->getForm();
-    //     $form->handleRequest($request);
+        $form = $this->createFormBuilder($figure)
+                    ->add('groupe', Category::class)
+                    ->add('name')
+                    ->add('image')
+                    ->add('content')
+                    ->getForm();
+        $form->handleRequest($request);
 
-    //     if ($form->isSubmitted() && $form->isValid()) {
-    //         $manager->persist($figure);
-    //         $manager->flush();
-    //         return $this->redirectToRoute('figure_show', ['id' => $figure->getId()]);
-    //     }
-    //     return $this->render('snowtricks/create.html.twig', [
-    //         'formFigure' => $form->createView()
-    //          ]);
-    // }
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($figure);
+            $manager->flush();
+            return $this->redirectToRoute('figure_show', ['id' => $figure->getId()]);
+        }
+        return $this->render('snowtricks/create.html.twig', [
+            'formFigure' => $form->createView()
+             ]);
+    }
 
     #[Route('/snowstricks/addfigure', name: 'add_figure')]
     public function addFigure(EntityManagerInterface $manager, Request $request): Response
     {
-        $user = $this->getUser();
+        //$user = $this->getUser();
         $figure = new Figure();
-        $figure->getUser($user);
-
         $form = $this->createForm(FigureType::class, $figure);
         $form->handleRequest($request);
-        $figure = $form->getData();
-        $manager->persist($figure);
-        $manager->flush();
+        if ($form->isSubmitted()) {
+
+            //$figure->getUser($user);
+            $figure = $form->getData();
+            $manager->persist($figure);
+            $manager->flush();
+        }
 
 
 
@@ -80,11 +84,15 @@ class SnowtricksController extends AbstractController
 
 
     #[Route('/snowtricks/{id}', name: 'figure_show')]
-    public function show($id, FigureRepository $repo): Response
+    public function show($id, FigureRepository $repo, UserRepository $use): Response
     {
+        $user = $this->getUser();
+
+
         $comment = new Comment();
         $form = $this->createForm(CommentType::class, $comment);
         $figure = $repo->find($id);
+
         return $this->render('snowtricks/show.html.twig', [
             'figure' => $figure,
             'commentForm' => $form->createView()
--- src/Form/FigureType.php
+++ src/Form/FigureType.php
@@ -6,6 +6,7 @@ use App\Entity\Figure;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 
 class FigureType extends AbstractType
 {
@@ -13,7 +14,7 @@ class FigureType extends AbstractType
     {
         $builder
             ->add('name')
-            ->add('image')
+            ->add('image', FileType::class)
             ->add('content')
             ->add('groupe')
             ->add('user')
