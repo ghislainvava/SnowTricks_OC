216fb9b0ece688cd4374f30a4309a6c299ecad21
ghislainvava <ghislainvachet@yahoo.fr>
2022-09-01 04:34:22 +0200

    pourquoi?

--- src/Form/VideoFormType 2.php
+++ src/Form/VideoFormType 2.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Video;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,8 +13,13 @@ class VideoFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('frame')
-           // ->add('figure')
+            ->add('frame', TextType::class, [
+                'label' => " saissisez le lien 'iframe' d'une vidéo Youtube : ",
+                'attr' => [
+                    'class' => 'form-control'
+
+                ]
+            ])
         ;
     }
 
